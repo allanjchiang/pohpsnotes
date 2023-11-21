e657cb3187d256e3693be31d9f9ae1953e0589fb
Allan Chiang <allanchiang24@gmail.com>
2023-11-21 17:28:08 +1300

    Remove center widget

--- pohpsnotes/lib/main.dart
+++ pohpsnotes/lib/main.dart
@@ -22,9 +22,7 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Register'),
       ),
-      body: Center(
-          child: TextButton(
-              onPressed: () async {}, child: const Text('Register'))),
+      body: TextButton(onPressed: () async {}, child: const Text('Register')),
     );
   }
 }
